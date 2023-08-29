dadac219db45c2156f4d3da293efa5fb9a0533c6
Anne Wynter <missannemary1@gmail.com>
2023-08-28 22:07:08 -0400

    fixing server.js by adding there connection to mongo db

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const db = require('./config/connection');
-// Require model
-// const { Item } = require('./models');
+const routes = require('./routes');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes); 
+// Connect to the MongoDB database and start the server
 
 db.once('open', () => {
   app.listen(PORT, () => {
